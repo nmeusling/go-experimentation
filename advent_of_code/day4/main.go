b11030d300ad3d183691b1ff57ab18fc983843f5
nmeusling <15682036+nmeusling@users.noreply.github.com>
2024-12-04 10:51:57 -0500

    day4 part 1 refactoring

--- advent_of_code/day4/main.go
+++ advent_of_code/day4/main.go
@@ -17,53 +17,50 @@ func main() {
 
 }
 
+func getPotentialMatches(grid []string, row, col int) []string {
+	potentialMatches := make([]string, 0)
+	if col+4 <= len(grid[row]) {
+		forward := grid[row][col : col+4]
+		potentialMatches = append(potentialMatches, forward)
+		if row+4 <= len(grid) {
+			down_right := ""
+			for k := 0; k < 4; k++ {
+				down_right += string(grid[row+k][col+k])
+			}
+			potentialMatches = append(potentialMatches, down_right)
+		}
+	}
+
+	if row+4 <= len(grid) {
+		down := ""
+		for k := 0; k < 4; k++ {
+			down += string(grid[row+k][col])
+		}
+		potentialMatches = append(potentialMatches, down)
+		if col-3 >= 0 {
+			down_left := ""
+			for k := 0; k < 4; k++ {
+				down_left += string(grid[row+k][col-k])
+			}
+			potentialMatches = append(potentialMatches, down_left)
+		}
+	}
+	return potentialMatches
+
+}
 func countXmas(grid []string) int {
 	target := "XMAS"
 	target_reversed := "SAMX"
 	count := 0
 	for i, row := range grid {
 		for j := range row {
-			// forward/backward match
-			if j+4 <= len(row) {
-				forward := grid[i][j : j+4]
-				if forward == target || forward == target_reversed {
+			potentialMatches := getPotentialMatches(grid, i, j)
+			for _, potentialMatch := range potentialMatches {
+				if potentialMatch == target || potentialMatch == target_reversed {
 					count++
 				}
 			}
 
-			//down/up match
-			if i+4 <= len(grid) {
-				down := ""
-				for k := 0; k < 4; k++ {
-					down += string(grid[i+k][j])
-				}
-				if down == target || down == target_reversed {
-					count++
-				}
-			}
-
-			//down right/up left
-			if j+4 <= len(row) && i+4 <= len(grid) {
-				down_right := ""
-				for k := 0; k < 4; k++ {
-					down_right += string(grid[i+k][j+k])
-				}
-				if down_right == target || down_right == target_reversed {
-					count++
-				}
-			}
-
-			//down left / up right
-			if j-3 >= 0 && i+4 <= len(grid) {
-				down_right := ""
-				for k := 0; k < 4; k++ {
-					down_right += string(grid[i+k][j-k])
-				}
-				// fmt.Println(down_right)
-				if down_right == target || down_right == target_reversed {
-					count++
-				}
-			}
 		}
 	}
 	return count
