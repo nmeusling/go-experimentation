1457cbd9208262113903ceddbb572b5a8d3b393a
nmeusling <15682036+nmeusling@users.noreply.github.com>
2024-12-06 13:50:34 -0500

    added prints for day 5 part 2

--- advent_of_code/day5/main.go
+++ advent_of_code/day5/main.go
@@ -62,16 +62,47 @@ func sumInvalid(rules map[int][]int, lines [][]int) int {
 	return count
 }
 
-// func reOrder(rules map[int][]int, line []int){
-// 	correct := []int{line[0]}
-// 	for i := 1; i < len(line); i++{
-// 		if
-// 	}
-// }
+func sumReordered(rules map[int][]int, lines [][]int) int {
+	count := 0
+	for _, line := range lines {
+		if !verifyOrder(rules, line) {
+			reOrder(rules, line)
+			count += line[len(line)/2]
+		}
+	}
+	return count
+}
+
+func reOrder(rules map[int][]int, line []int) []int {
+	for i := len(line) - 1; i >= 0; i-- {
+		swapIndex := hasOtherItemAfter(rules, line[i], line[:i])
+		for swapIndex >= 0 {
+			temp := line[i]
+			line[i] = line[swapIndex]
+			line[swapIndex] = temp
+			swapIndex = hasOtherItemAfter(rules, line[i], line[:i])
+		}
+	}
+	return line
+}
+
+func hasOtherItemAfter(rules map[int][]int, value int, before []int) int {
+	for i, beforeValue := range before {
+		//one of the values that should be after is before
+		if slices.Contains(rules[value], beforeValue) {
+			return i
+
+		}
+	}
+	return -1
+}
 
 func main() {
 	rulesSam, manualsSam := getInput("day5_sample")
 	fmt.Printf("Sample sum: %v\n", sumInvalid(rulesSam, manualsSam))
 	rules, manuals := getInput("day5_data")
 	fmt.Printf("Sum: %v", sumInvalid(rules, manuals))
+	fmt.Printf("Sample reorder %v\n", sumReordered(rulesSam, manualsSam))
+	fmt.Printf("Reorder %v\n", sumReordered(rules, manuals))
+
 }
