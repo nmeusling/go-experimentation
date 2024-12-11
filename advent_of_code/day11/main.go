0e1e56d6c019aa660d4c8181704256e04e445564
nmeusling <15682036+nmeusling@users.noreply.github.com>
2024-12-11 11:39:04 -0500

    day 11 pt 2

--- advent_of_code/day11/main.go
+++ advent_of_code/day11/main.go
@@ -10,16 +10,19 @@ import (
 
 func main() {
 	sampleStones := getStones("day11_sample")
-	sampleStones = stepStoneNTimes(25, sampleStones)
+	sampleStones = stepStonesNTimes(25, sampleStones)
 	fmt.Println(len(sampleStones))
 
 	stones := getStones("day11_data")
-	stones = stepStoneNTimes(25, stones)
+	stones = stepStonesNTimes(25, stones)
 	fmt.Println(len(stones))
 
-	stones2 := getStones("day11_data")
-	stones2 = stepStoneNTimes(50, stones2)
-	fmt.Println(len(stones2))
+	sampleStones2 := getInitialStones("day11_sample")
+	processed := stepMappedStonesNTimes(sampleStones2, 75)
+	fmt.Println(countStones(processed))
+
+	stones2 := getInitialStones("day11_data")
+	fmt.Println(countStones(stepMappedStonesNTimes(stones2, 75)))
 }
 
 func getStones(fileName string) []int {
@@ -32,15 +35,15 @@ func getStones(fileName string) []int {
 	return stones
 }
 
-func stepStoneNTimes(n int, stones []int) []int {
+func stepStonesNTimes(n int, stones []int) []int {
 	newStones := stones
 	for i := 0; i < n; i++ {
-		newStones = stepStone(newStones)
+		newStones = stepStones(newStones)
 	}
 	return newStones
 }
 
-func stepStone(stones []int) []int {
+func stepStones(stones []int) []int {
 	newStones := []int{}
 	for _, stone := range stones {
 		if stone == 0 {
@@ -65,3 +68,49 @@ func splitStone(stone int) []int {
 	second, _ := strconv.Atoi(digits[mid:])
 	return []int{first, second}
 }
+
+func getInitialStones(fileName string) map[int]int {
+	stones := make(map[int]int)
+	for _, s := range getStones(fileName) {
+		stones[s]++
+	}
+	return stones
+}
+
+func stepMappedStones(stones map[int]int) map[int]int {
+	newStones := make(map[int]int)
+	for s, count := range stones {
+		results := stepStones([]int{s})
+		for _, r := range results {
+			newStones[r] += count
+		}
+	}
+	return newStones
+}
+
+func stepMappedStonesNTimes(stones map[int]int, n int) map[int]int {
+	newStones := stones
+	for i := 0; i < n; i++ {
+		newStones = stepMappedStones(newStones)
+	}
+	return newStones
+}
+
+func countStones(stones map[int]int) int {
+	numRocks := 0
+	for _, count := range stones {
+		numRocks += count
+	}
+	return numRocks
+}
+
+// 0->1->2024->20, 24->2, 0, 2,4 ->
+// 1->2024->20, 24->2, 0, 2,4 ->
+//2->4048->40, 48-> 4, 0, 4, 8
+//3 -> 6072 -> 60, 72 -> 6, 0, 7, 2
+//4 -> 8096 -> 80, 96 -> 8,0,9,6
+//5 -> 10120 - 20482880 -> 2048, 2880 -> 20, 48, 28, 80 ->2,0,4,8,2,8,8,0
+//6 -> 12144 -> 24579456 -> 2457, 9456 -> 24, 57, 94, 56 -> 2,4,5,7,9,4,5,6
+//7 -> 14168 -> 28676032 -> 2867, 6032 -> 28, 67, 60, 32 -> 2, 8, 6, 7, 6, 0, 3, 2
+//8 -> 16192 -> 32772608 -> 3277, 2608 -> 32, 77, 26, 8 -> 3, 2, 7, 7, 2, 6, 16192
+//9 -> 18216 -> 36869184 -> 3686, 9184 -> 36, 86, 91, 84 -> 3,6,8,6,9,1,8,4
