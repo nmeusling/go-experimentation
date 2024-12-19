47d24827ca6ae4df940efb9e108a7f9a9290a0c3
nmeusling <15682036+nmeusling@users.noreply.github.com>
2024-12-19 14:09:08 -0500

    add pq to devenv

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,7 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [ pkgs.git pkgs.jq ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
